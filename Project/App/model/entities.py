e3924aff8ef0bf2691d7569f427b1cfb99bc4577
JoseChavez98 <jose.chavez@utec.edu.pe>
2018-06-09 16:57:06 -0500

    update

--- Project/App/model/entities.py
+++ Project/App/model/entities.py
@@ -3,9 +3,9 @@ from database import connector
 
 class User(connector.Manager.Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    fullname = Column(String(50))
+    #id = Column(Integer, primary_key=True)
+    name = Column(String(50),primary_key=True)
+    email = Column(String(50))
     password = Column(String(12))
 
 
--- Project/App/server.py
+++ Project/App/server.py
@@ -29,12 +29,8 @@ def hello_world():
 
 @app.route('/logout',methods=['POST'])
 def log_out():
+    print('sali')
     return render_template('login.html')
-
-##############################################################################
-################# USERS ######################################################
-##############################################################################
-
 
 @app.route('/dologin',  methods = ['POST'])
 def do_login():
@@ -46,19 +42,28 @@ def do_login():
         print('\n')
         print(user.name)
 
-        if user.name == data['username'] and user.password == data['password']:
+        if (user.name == data['username'] or user.email == data['username']) and user.password == data['password']:
             return render_template('home.html')
 
     return render_template('login.html')
 
+@app.route('/showsignup',methods=['POST'])
+def show_signup():
+    return render_template('register.html')
 
+@app.route('/showlogin',methods=['POST'])
+def show_login():
+    return render_template('login.html')
+##############################################################################
+################# USERS ######################################################
+##############################################################################
 
 
 @app.route('/setUsers')
 def set_user():
 
-    user1 = entities.User(id=1, name='jose', fullname='jose chavez', password='1234')
-    user2 = entities.User(id=2, name='guille', fullname='guillermo', password='1234')
+    user1 = entities.User( name='jose', email='jose.chavez@utec.edu.pe', password='1234')
+    user2 = entities.User( name='guille', email='guillermo.franco@utec.edu.pe', password='1234')
     session = db.getSession(engine)
     session.add(user1)
     session.add(user2)
@@ -66,44 +71,44 @@ def set_user():
     return 'Created users'
 
 
-@app.route('/users', methods = ['GET'])
-def get_users():
-    key = 'getUsers'
-    if key not in cache.keys():
-        session = db.getSession(engine)
-        dbResponse = session.query(entities.User)
-        cache[key] = dbResponse;
-        print("From DB")
-    else:
-        print("From Cache")
+#@app.route('/users', methods = ['GET'])
+#def get_users():
+#    key = 'getUsers'
+#    if key not in cache.keys():
+ #       session = db.getSession(engine)
+  #     dbResponse = session.query(entities.User)
+   #     cache[key] = dbResponse;
+    #    print("From DB")
+    #else:
+     #   print("From Cache")
+#
+ #   users = cache[key];
+  #  response = []
+   # for user in users:
+    #    response.append(user)
+    #return json.dumps(response, cls=connector.AlchemyEncoder)
 
-    users = cache[key];
-    response = []
-    for user in users:
-        response.append(user)
-    return json.dumps(response, cls=connector.AlchemyEncoder)
+#@app.route('/users/<id>', methods = ['GET'])
+#def get_user(id):
+#    session = db.getSession(engine)
+#    users = session.query(entities.User).filter(entities.User.id == id)
+#    for user in users:
+#        js = json.dumps(user, cls=connector.AlchemyEncoder)
+#        return  Response(js, status=200, mimetype='application/json')
 
-@app.route('/users/<id>', methods = ['GET'])
-def get_user(id):
-    session = db.getSession(engine)
-    users = session.query(entities.User).filter(entities.User.id == id)
-    for user in users:
-        js = json.dumps(user, cls=connector.AlchemyEncoder)
-        return  Response(js, status=200, mimetype='application/json')
-
-    message = { "status": 404, "message": "Not Found"}
-    return Response(message, status=404, mimetype='application/json')
+#    message = { "status": 404, "message": "Not Found"}
+#    return Response(message, status=404, mimetype='application/json')
 
 
-@app.route('/users', methods = ['DELETE'])
-def remove_user():
-    id=request.form['key']
-    session = db.getSession(engine)
-    users = session.query(entities.User).filter(entities.User.id == id)
-    for user in users:
-        session.delete(user)
-    session.commit()
-    return "DELETED"
+#@app.route('/users', methods = ['DELETE'])
+#def remove_user():
+#    id=request.form['key']
+#    session = db.getSession(engine)
+#    users = session.query(entities.User).filter(entities.User.id == id)
+#    for user in users:
+#        session.delete(user)
+#    session.commit()
+#    return "DELETED"
 
 
 @app.route('/users', methods = ['POST'])
@@ -111,9 +116,8 @@ def create_user():
     c = json.loads(request.form['values'])
     print(c)
     user = entities.User(
-        id=c['id'],
         name=c['name'],
-        fullname=c['fullname'],
+        email=c['fullname'],
         password=c['password']
     )
     session = db.getSession(engine)
@@ -121,17 +125,17 @@ def create_user():
     session.commit()
     return 'Created users'
 
-@app.route('/users', methods = ['PUT'])
-def update_user():
-    session = db.getSession(engine)
-    id = request.form['key']
-    user = session.query(entities.User).filter(entities.User.id == id).first()
-    c =  json.loads(request.form['values'])
-    for key in c.keys():
-        setattr(user, key, c[key])
-    session.add(user)
-    session.commit()
-    return ('Updated User')
+#@app.route('/users', methods = ['PUT'])
+#def update_user():
+#    session = db.getSession(engine)
+#    id = request.form['key']
+#    user = session.query(entities.User).filter(entities.User.id == id).first()
+#    c =  json.loads(request.form['values'])
+#    for key in c.keys():
+#        setattr(user, key, c[key])
+#    session.add(user)
+#    session.commit()
+#    return ('Updated User')
 
 
 
@@ -143,7 +147,7 @@ def update_user():
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-@app.route('/show',methods=['POST'])
+@app.route('/showupload',methods=['POST'])
 def show():
     return render_template('upload.html')
 @app.route('/upload', methods=['GET','POST'])
